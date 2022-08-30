e1f9748142c6d27ba2eba8a009477ef4735b9e1f
Morgan Githinji <githinjimorgan@gmail.com>
2022-08-30 15:22:34 -0700

    Adding GenCopy to GitHub

---
+++ gencopy/GenCopyAPI.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+from GenCopy import generate_tagline, generate_keywords, MAX_INPUT_LENGTH
+
+app = FastAPI()
+
+# API for generating a tagline
+@app.get("/generate_tagline")
+async def generate_tagline_api(prompt: str):
+    """API for AI-generated (GPT-3) business taglines based on user prompt."""
+    validate_prompt(prompt=prompt)
+    tagline = generate_tagline(prompt=prompt)
+    return {"tagline": tagline, "keywords": []}
+
+# API for generating keywords
+@app.get("/generate_keywords")
+async def generate_keywords_api(prompt: str):
+    """API for AI-generated (GPT-3) business keywords based on user prompt."""
+    validate_prompt(prompt=prompt)
+    keywords = generate_keywords(prompt=prompt)
+    return {"tagline": None, "keywords": keywords}
+
+# API for generating tagline and keywords
+@app.get("/generate_tagline_and_keywords")
+async def generate_tagline_and_keywords_api(prompt: str):
+    """API for AI-generated (GPT-3) business tagline and keywords based on user prompt."""
+    validate_prompt(prompt=prompt)
+    tagline = generate_tagline(prompt=prompt)
+    keywords = generate_keywords(prompt=prompt)
+    return {"tagline": tagline, "keywords": keywords}
+
+# validate the user's prompt is acceptable
+def validate_prompt(prompt: str):
+    """Validation function for ensuring user's prompt meets acceptance criteria"""
+    
+    # validating prompt length
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(status_code=400, detail="Prompt exceeds max length of {} characters.".format(MAX_INPUT_LENGTH))
+
+# To make the API live, run the following in the terminal
+# uvicorn GenCopyAPI:app --reload
---
+++ gencopy/openAITest.py
@@ -0,0 +1,12 @@
+import os
+from urllib import response
+import openai
+
+openai.api_key = os.getenv('OPEN_AI_KEY')
+
+subject = "coffee"
+# prompt = "Write a confident tagline for a {} shop:".format(subject)
+prompt = "Write a sexy R&B song."
+
+response = openai.Completion.create(model="text-davinci-002", prompt=prompt, max_tokens=500, temperature=0.11)
+print(response["choices"][0]["text"])
