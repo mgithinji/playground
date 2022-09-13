ceae5e9809fe2593b1dc32227903b20cc49cc099
Morgan Githinji <githinjimorgan@gmail.com>
2022-09-12 17:15:54 -0700

    Experimenting with the different models

--- gencopy/openAITest.py
+++ gencopy/openAITest.py
@@ -2,11 +2,12 @@ import os
 from urllib import response
 import openai
 
-openai.api_key = os.getenv('OPEN_AI_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-subject = "coffee"
-# prompt = "Write a confident tagline for a {} shop:".format(subject)
+# subject = "coffee"
+# prompt = "Write a confident tagline for a {} business:".format(subject)
 prompt = "Write an worship hymn:"
 
 response = openai.Completion.create(model="text-davinci-002", prompt=prompt, max_tokens=500, temperature=0.11)
+# response = openai.Completion.create(model="text-ada-001", prompt=prompt, max_tokens=30, temperature=0.60)
 print(response["choices"][0]["text"])
